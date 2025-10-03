896461c75aada636728434fb1fd949d427fb9dae
KasumiMercury <naoki_shira@outlook.jp>
2025-10-03 23:33:56 +0900

    chore: setup biome

--- src/App.tsx
+++ src/App.tsx
@@ -3,50 +3,52 @@ import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 interface FileInfo {
-	path: string;
-	is_directory: boolean;
-	is_symlink: boolean;
-	size: number;
-	modified: string | null;
+  path: string;
+  is_directory: boolean;
+  is_symlink: boolean;
+  size: number;
+  modified: string | null;
 }
 
 function App() {
-	const [paths, setPaths] = useState<FileInfo[]>([]);
-	const [loading, setLoading] = useState(false);
+  const [paths, setPaths] = useState<FileInfo[]>([]);
+  const [loading, setLoading] = useState(false);
 
-	async function scanDirectory() {
-		setLoading(true);
-		try {
-			invoke<FileInfo[]>("scan_current_directory")
-				.then((res) => {
-					console.log(res);
-					setPaths(res);
-					setLoading(false);
-				})
-				.catch((err) => {
-					console.error("Invocation error:", err);
-					setPaths([]);
-					setLoading(false);
-				});
-		} catch (error) {
-			console.error("Failed to scan directory:", error);
-			setPaths([]);
-		}
-	}
+  async function scanDirectory() {
+    setLoading(true);
+    try {
+      invoke<FileInfo[]>("scan_current_directory")
+        .then((res) => {
+          console.log(res);
+          setPaths(res);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.error("Invocation error:", err);
+          setPaths([]);
+          setLoading(false);
+        });
+    } catch (error) {
+      console.error("Failed to scan directory:", error);
+      setPaths([]);
+    }
+  }
 
-	return (
-		<main className="container">
-			<button onClick={scanDirectory}>Scan</button>
-			{loading && <div>Loading...</div>}
-			<div style={{ marginTop: "20px" }}>
-				{paths.map((item, index) => (
-					<div key={index}>
-						{item.path}/ / size:{item.size} / modified: {item.modified} / is_directory: {item.is_directory.toString()} / is_symlink: {item.is_symlink.toString()}
-					</div>
-				))}
-			</div>
-		</main>
-	);
+  return (
+    <main className="container">
+      <button onClick={scanDirectory}>Scan</button>
+      {loading && <div>Loading...</div>}
+      <div style={{ marginTop: "20px" }}>
+        {paths.map((item, index) => (
+          <div key={index}>
+            {item.path}/ / size:{item.size} / modified: {item.modified} /
+            is_directory: {item.is_directory.toString()} / is_symlink:{" "}
+            {item.is_symlink.toString()}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default App;
