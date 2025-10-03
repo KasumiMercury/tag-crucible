b34a88da340d1c0a4d56570673c07b5d0173dcbf
KasumiMercury <naoki_shira@outlook.jp>
2025-10-04 00:02:28 +0900

    feat: setup Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -35,10 +35,15 @@ function App() {
   }
 
   return (
-    <main className="container">
-      <button onClick={scanDirectory}>Scan</button>
+    <main className="pt-[10vh] flex flex-col justify-center text-center">
+      <button
+        onClick={scanDirectory}
+        className="rounded-lg border border-transparent px-5 py-2.5 text-base font-medium bg-white text-gray-950 shadow-md transition-colors duration-200 cursor-pointer hover:border-blue-600 active:border-blue-600 active:bg-gray-200 dark:text-white dark:bg-gray-950/60 dark:active:bg-gray-950/40 outline-none"
+      >
+        Scan
+      </button>
       {loading && <div>Loading...</div>}
-      <div style={{ marginTop: "20px" }}>
+      <div className="mt-5">
         {paths.map((item, index) => (
           <div key={index}>
             {item.path}/ / size:{item.size} / modified: {item.modified} /
